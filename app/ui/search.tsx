8d8e891792d5da17d68059f7568c5d8d7a378a85
Aadi Vinayak <aadivinayak36@gmail.com>
2025-05-07 00:23:59 +0530

    feat: implement pagination and search functionality in invoices page; enhance data fetching and URL handling

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -5,12 +5,14 @@ import { useSearchParams,usePathname, useRouter } from 'next/navigation';
 
 
 export default function Search({ placeholder }: { placeholder: string }) {
-
+  
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
   function handleSearch(term: string) {
+    console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if(term){
       params.set('query',term);
     }
